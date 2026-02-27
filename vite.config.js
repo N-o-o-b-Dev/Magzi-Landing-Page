39b06471cab1a4f8f47cb8cf3d818cd27dc66ea7
N-o-o-b-Dev <140104064+N-o-o-b-Dev@users.noreply.github.com>
2026-02-27 18:22:32 +0530

    gh-page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Magzi-landing-page/',
+  base: '/Magzi-Landing-Page/',
 })
